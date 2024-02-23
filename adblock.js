1ee8c9e8ad6878084e27b94930561b8affe5ef2b
siritami <103488094+siritami@users.noreply.github.com>
2024-02-23 10:01:15 +0700

    Update adblock.js
    Signed-off-by: siritami <103488094+siritami@users.noreply.github.com>

--- adblock.js
+++ adblock.js
@@ -1,1 +1,109 @@
-var currentTime,duration,yt;function myWindow(){let e=window.location.href.replace("youtube","youtube5s");window.open(e,"Download Youtube Video","directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800, height=900").onload=setInterval(clearPage,1e3)}function clearPage(){try{document.querySelectorAll(".col-xs-12")[8].remove(),document.querySelector("footer").remove(),document.querySelector("ul").remove(),document.querySelector(".navbar-header").remove(),document.querySelector("#logo-icon").remove()}catch(e){return}}function createButton(){try{let e=document.createElement("style");e.innerHTML="\n    .myButton {\n    font-size: 14px;\n    font-weight: 300;\n    color: #eee;\n    text-align: center;\n    vertical-align: middle;\n    border: 1px solid #555;\n    border-radius: 50px;\n    background-color: rgba(0, 0, 0, 0);\n    height: 36px;\n    width: 135px;\n    padding: 0;\n    margin: 8px\n    }\n    ",document.head.appendChild(e);let t=document.createElement("BUTTON");t.className="myButton",t.id="mybutton",t.innerHTML="DOWNLOAD",t.addEventListener("click",myWindow),document.querySelector("#owner").appendChild(t)}catch(e){}}function adOverlay(){const e=document.querySelector(".ytp-ad-text"),t=document.querySelector("video");e&&(t.currentTime=t.duration),document.querySelector(".ytp-ad-skip-button-icon-modern")&&(document.querySelector(".ytp-ad-skip-button-icon-modern").click(),console.log("Skipped Video Ad"))}function removeSp(){try{1==document.getElementById("support").innerText.includes("Ad")&&(document.getElementsByClassName("ytd-rich-item-renderer")[0].innerHTML="",console.log("Sponsor Ad removed!"))}catch(e){return}}var Ads={aId:["masthead-ad","player-ads","top-container","offer-module","pyv-watch-related-dest-url","ytd-promoted-video-renderer","sparkles-container","fulfilled-layout"],aClass:["style-scope ytd-search-pyv-renderer","ytd-compact-promoted-video-renderer","style-scope ytd-carousel-ad-renderer","ytp-ad-overlay-container","ytp-ad-message-container"],aTag:["ytd-promoted-sparkles-text-search-renderer"],vdoAd:["ytp-ad-text ytp-ad-preview-text","ytp-ad-skip-button ytp-button"],removeByID:function(){this.aId.forEach((e=>{let t=document.getElementById(e);t&&t.remove()}))},removeByClassName:function(){this.aClass.forEach((e=>{let t=document.getElementsByClassName(e);t[0]&&t[0].remove()}))},removeByTagName:function(){this.aTag.forEach((e=>{let t=document.getElementsByTagName(e);t[0]&&t[0].remove()}))},removeVdoAd:function(){this.vdoAd.forEach((e=>{let t=document.getElementsByClassName(e)[0];t&&(t.click(),console.log("Skipped a clickable Ad"))}))}};function killAd(){Ads.removeByID(),Ads.removeByClassName(),Ads.removeByTagName(),Ads.removeVdoAd(),removeSp(),adOverlay()}document.addEventListener("DOMContentLoaded",(()=>{setInterval(killAd,100),setTimeout(createButton,5e3)}));
+var currentTime, duration, yt;
+ 
+ 
+function myWindow()
+{
+    let y = window.location.href.replace("youtube", "youtube5s");
+    let myWin = window.open(y,"Download Youtube Video","directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800, height=900");
+    myWin.onload = setInterval(clearPage,1000);
+}
+ 
+function clearPage()
+{
+    try{
+        document.querySelectorAll(".col-xs-12")[8].remove();
+        document.querySelector("footer").remove();
+        document.querySelector("ul").remove();
+        document.querySelector(".navbar-header").remove();
+        document.querySelector("#logo-icon").remove();
+    }
+    catch(e)
+    {
+        return;
+    }
+}
+ 
+ 
+function createButton()
+{
+    try{
+    let css = document.createElement('style');
+    //Thanks GreenSheep3 for the css style
+    css.innerHTML = `
+    .myButton {
+    font-size: 14px;
+    font-weight: 300;
+    color: #eee;
+    text-align: center;
+    vertical-align: middle;
+    border: 1px solid #555;
+    border-radius: 50px;
+    background-color: rgba(0, 0, 0, 0);
+    height: 36px;
+    width: 135px;
+    padding: 0;
+    margin: 8px
+    }
+    `;
+    document.head.appendChild(css);
+    let btn = document.createElement("BUTTON");
+    btn.className = "myButton";
+    btn.id = "mybutton";
+    btn.innerHTML = "DOWNLOAD";
+    btn.addEventListener("click", myWindow);
+    document.querySelector("#owner").appendChild(btn);
+    }
+    catch(e)
+    {
+    }
+}
+ 
+function adOverlay(){
+const ad = document.querySelector(".ytp-ad-text");
+const video = document.querySelector("video");
+if (ad){
+video.currentTime = video.duration;
+}
+let btn = document.querySelector(".ytp-ad-skip-button-icon-modern");
+if(btn){document.querySelector(".ytp-ad-skip-button-icon-modern").click();console.log("Skipped Video Ad");}
+}
+ 
+ 
+function removeSp()
+{
+    try
+    {
+        if (document.getElementById("support").innerText.includes("Ad") == true)
+        {
+            document.getElementsByClassName("ytd-rich-item-renderer")[0].innerHTML = ""
+            console.log('Sponsor Ad removed!');
+        }
+    }
+    catch(e)
+    {
+        return;
+    }
+}
+ 
+var Ads = {
+    "aId":["masthead-ad","player-ads","top-container","offer-module","pyv-watch-related-dest-url","ytd-promoted-video-renderer","sparkles-container","fulfilled-layout"],
+    "aClass":["style-scope ytd-search-pyv-renderer","ytd-compact-promoted-video-renderer","style-scope ytd-carousel-ad-renderer","ytp-ad-overlay-container","ytp-ad-message-container"],
+    "aTag":["ytd-promoted-sparkles-text-search-renderer"],
+    "vdoAd":["ytp-ad-text ytp-ad-preview-text","ytp-ad-skip-button ytp-button"],
+    "removeByID":function(){this.aId.forEach(i=>{ let AdId = document.getElementById(i);if(AdId) AdId.remove();})},
+    "removeByClassName":function(){this.aClass.forEach(c=>{ let AdClass = document.getElementsByClassName(c);if(AdClass[0]) AdClass[0].remove();})},
+    "removeByTagName":function(){this.aTag.forEach(t=>{ let AdTag = document.getElementsByTagName(t);if(AdTag[0]) AdTag[0].remove();})},
+    "removeVdoAd":function(){this.vdoAd.forEach(v=>{let AdVdo = document.getElementsByClassName(v)[0];if(AdVdo) {AdVdo.click(); console.log("Skipped a clickable Ad")}})} //handles skippable video Ad
+}
+ 
+function killAd()
+{
+    Ads.removeByID();
+    Ads.removeByClassName();
+    Ads.removeByTagName();
+    Ads.removeVdoAd();
+    removeSp();
+    adOverlay();
+}
+ 
+document.addEventListener('DOMContentLoaded', ()=>{setInterval(killAd, 100); setTimeout(createButton, 5000);});
